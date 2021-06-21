4c467b4a6605f6a848a60a27238b7d502c0d9a44
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-06-20 23:56:07 -0300

    change Authetication

--- lib/app/domain/usecases/authentication.dart
+++ lib/app/domain/usecases/authentication.dart
@@ -1,5 +1,13 @@
 import '../entities/entities.dart';
 
 abstract class Authetication {
-  Future<AccountEntity> auth({required String email, required String password});
+  Future<AccountEntity> auth({required AuthenticationParams params});
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({required this.email, required this.secret});
+
 }
