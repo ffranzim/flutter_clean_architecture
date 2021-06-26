12bd5755b0ef4cae5661f60e9981bd80ee3e32bf
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-06-26 00:09:22 -0300

    Funcional sem nullsafety

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -3,10 +3,10 @@ import 'package:clean_architecture/app/data/usecases/remote_authetication.dart';
 import 'package:clean_architecture/app/domain/helpers/helpers.dart';
 import 'package:clean_architecture/app/domain/usecases/authentication.dart';
 
+import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:faker/faker.dart';
 
-import 'package:mocktail/mocktail.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -16,11 +16,11 @@ void main() {
   String url;
   AuthenticationParams params;
 
-  When<Future<Map<dynamic, dynamic>>> mockRequest() =>
-      when(() => httpClient.request(
-          url: any(named: 'url'),
-          method: any(named: 'method'),
-          body: any(named: 'body')));
+  PostExpectation mockRequest() =>
+      when( httpClient.request(
+          url: anyNamed('url'),
+          method: anyNamed('method'),
+          body: anyNamed('body')));
 
   void mockHttpData(Map data) {
     mockRequest().thenAnswer((_) async => data);
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockito/mockito.dart';
 import 'package:faker/faker.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -18,13 +18,19 @@ class HttpAdapter {
     'accept': 'application/json',
   };
 
-  Future<void> request({
+  Future<Map> request({
     @required Uri url,
     @required String method,
     Map body,
   }) async {
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(url, headers: headers, body: jsonBody);
+    final response = await client.post(url, headers: headers, body: jsonBody);
+
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
@@ -42,6 +48,18 @@ void main() {
   });
 
   group('post', () {
+    PostExpectation mockRequest() => when(
+        client.post(url, body: anyNamed('body'), headers: anyNamed('headers')));
+
+    void mockResponse(int statusCode,
+        {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
     test('Should call post with correct values', () async {
       await sut.request(
         url: url,
@@ -50,7 +68,7 @@ void main() {
       );
 
       verify(
-        () => client.post(
+        client.post(
           url,
           headers: {
             'content-type': 'application/json',
@@ -68,13 +86,41 @@ void main() {
       );
 
       verify(
-        () => client.post(
+        client.post(
           url,
-          headers: any(named: 'headers'),
+          headers: anyNamed('headers'),
         ),
       );
     });
 
+    test('Should return data if post returns 200', () async {
+      final response = await sut.request(url: url, method: 'post');
 
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    // test('Should return null if post returns 200 with no data', () async {
+    //   mockResponse(200, body: '');
+    //
+    //   final response = await sut.request(url: url, method: 'post');
+    //
+    //   expect(response, null);
+    // });
+    //
+    // test('Should return null if post returns 204', () async {
+    //   mockResponse(204, body: '');
+    //
+    //   final response = await sut.request(url: url, method: 'post');
+    //
+    //   expect(response, null);
+    // });
+    //
+    // test('Should return null if post returns 204 with data', () async {
+    //   mockResponse(204);
+    //
+    //   final response = await sut.request(url: url, method: 'post');
+    //
+    //   expect(response, null);
+    // });
   });
 }
