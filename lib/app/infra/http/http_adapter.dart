d013976bea890e0eb05385ee1df439484251842c
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-06-26 14:13:56 -0300

    move httpAdapter to its own file

---
+++ lib/app/infra/http/http_adapter.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  final headers = {
+    'content-type': 'application/json',
+    'accept': 'application/json',
+  };
+
+  @override
+  Future<Map> request(
+      {@required Uri url, @required String method, Map body}) async {
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(url, headers: headers, body: jsonBody);
+
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+}
