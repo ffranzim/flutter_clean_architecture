56e2f5a3bf76d817455422cf405dcf150efe8a2f
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-07-10 21:30:23 -0300

    Encerrando essa branch pois o curso continua com getx com stream

--- lib/app/main/main.dart
+++ lib/app/main/main.dart
@@ -23,6 +23,7 @@ class App extends StatelessWidget {
       initialRoute: '/login',
       getPages: [
         GetPage(name: '/login', page: makeLoginPage),
+        GetPage(name: '/surveys', page: () => const Scaffold(body: Text('Enquetes'),)),
       ],
     );
   }
