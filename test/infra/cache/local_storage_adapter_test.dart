a576046292ebeba6a364c26436b41ffdeccc82a8
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-07-13 22:21:23 -0300

    ensure LocalStorageAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,4 +1,5 @@
 import 'package:clean_architecture/app/data/cache/cache.dart';
+import 'package:clean_architecture/app/domain/helpers/domain_error.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -6,7 +7,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class LocalStorageAdapter implements SaveSecureCacheStorage {
-
   final FlutterSecureStorage secureStorage;
 
   LocalStorageAdapter({@required this.secureStorage});
@@ -17,9 +17,7 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
   }
 }
 
-class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {
-
-}
+class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
 void main() {
   FlutterSecureStorageSpy secureStorage;
@@ -27,17 +25,29 @@ void main() {
   String key;
   String value;
 
-  setUp((){
+  setUp(() {
     secureStorage = FlutterSecureStorageSpy();
     sut = LocalStorageAdapter(secureStorage: secureStorage);
     key = faker.lorem.word();
     value = faker.guid.guid();
   });
 
-  test('Should call save secure with correct values', () async {
+  void mockSaveSecureError() =>
+      when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+          .thenThrow(Exception());
 
+  test('Should call save secure with correct values', () async {
     await sut.save(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save secure throws', () async {
+
+    mockSaveSecureError();
+    final future = sut.save(key: key, value: value);
+
+    // ! TypeMatcher verifica se é do mesmo tipo - Tipo um instaceOf
+    expect(future, throwsA(const TypeMatcher<Exception>()));
   });
 }
