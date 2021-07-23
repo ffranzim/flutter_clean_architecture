8427f109d6d28e859f36e76b037e120f375b34a8
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-07-22 22:50:12 -0300

    ensure RemotAddAccount throws UnexpectedError on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -57,4 +57,10 @@ void main() {
     final future = sut.add(params: params);
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+    final future = sut.add(params: params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
