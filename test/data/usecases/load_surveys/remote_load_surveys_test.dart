b3fce9be01d133d33d9e30e264ba2762b9600145
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-08-06 21:43:45 -0300

    ensure RemoteLoadSurveys throws UnexpectedError on 404

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -58,6 +58,10 @@ void main() {
     mockRequest().thenAnswer((_) async => data);
   }
 
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     url = Uri.parse(faker.internet.httpUrl());
     httpClient = HttpClientSpy();
@@ -96,4 +100,11 @@ void main() {
     final future = sut.load();
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+    final future = sut.load();
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
