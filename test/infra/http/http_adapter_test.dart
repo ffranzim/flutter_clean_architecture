0e1e57c2d30643bda741b3d6b62c2afee586224d
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-08-07 22:06:38 -0300

    ensure HttpAdapter throws on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -227,23 +227,23 @@ void main() {
 
         expect(response, null);
       });
-    //
-    //   test('Should return BadRequestError if post returns 400 without body',
-    //       () async {
-    //     mockResponse(400, body: '');
-    //
-    //     final future = sut.request(url: url, method: 'post');
-    //
-    //     expect(future, throwsA(HttpError.badRequest));
-    //   });
-    //
-    //   test('Should return BadRequestError if post returns 400', () async {
-    //     mockResponse(400);
-    //
-    //     final future = sut.request(url: url, method: 'post');
-    //
-    //     expect(future, throwsA(HttpError.badRequest));
-    //   });
+
+      test('Should return BadRequestError if get returns 400 without body',
+          () async {
+        mockResponse(400, body: '');
+
+        final future = sut.request(url: url, method: 'get');
+
+        expect(future, throwsA(HttpError.badRequest));
+      });
+
+      test('Should return BadRequestError if get returns 400', () async {
+        mockResponse(400);
+
+        final future = sut.request(url: url, method: 'get');
+
+        expect(future, throwsA(HttpError.badRequest));
+      });
     //
     //   test('Should return BadRequestError if post returns 401', () async {
     //     mockResponse(401);
