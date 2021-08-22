a465ca80f705680290ef32f6f986d3adbf8f3c8b
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-08-21 21:38:34 -0300

    ensure LocalLoadSurveys returns a list of Surveys on success

---
+++ lib/app/data/models/local_survey_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+
+import '../../domain/entities/entities.dart';
+
+class LocalSurveyModel {
+  final String id;
+  final String question;
+  final DateTime date;
+  final bool didAnswer;
+
+  LocalSurveyModel({
+    @required this.id,
+    @required this.question,
+    @required this.date,
+    @required this.didAnswer,
+  });
+
+  factory LocalSurveyModel.fromJson(Map json) {
+
+    // if (!json.keys.toSet().containsAll(['id', 'question', 'date', 'didAnswer'])) {
+    //   throw HttpError.invalidData;
+    // }
+
+    return LocalSurveyModel(
+      id: json['id'] as String,
+      question: json['question'] as String,
+      date: json['dateTime'] as DateTime,
+      didAnswer: json['didAnswer'] as bool,
+    );
+  }
+
+  SurveyEntity toEntity() => SurveyEntity(
+        id: id,
+        question: question,
+        // dateTime: DateTime.parse(date),
+        dateTime: date,
+        didAnswer: didAnswer,
+      );
+}
--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,3 +1,6 @@
+import 'package:clean_architecture/app/data/models/models.dart';
+import 'package:clean_architecture/app/domain/entities/entities.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -7,13 +10,18 @@ class LocalLoadSurveys {
 
   LocalLoadSurveys({@required this.fetchCacheStorage});
 
-  Future<void> load() async {
-    await fetchCacheStorage.fetch('surveys');
+  Future<List<SurveyEntity>> load() async {
+    final data = await fetchCacheStorage.fetch('surveys');
+
+    // ignore: argument_type_not_assignable
+    final surveysDynamic = data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+    final surveysEntity = (surveysDynamic as List<dynamic>).cast<SurveyEntity>();
+    return surveysEntity;
   }
 }
 
 abstract class FetchCacheStorage {
-  Future<void> fetch(String key);
+  Future<dynamic> fetch(String key);
 }
 
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
@@ -21,17 +29,56 @@ class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
 void main() {
   FetchCacheStorage fetchCacheStorage;
   LocalLoadSurveys sut;
+  List<Map> data;
+
+  //? DateTime.utc(2020, 7, 20)
+  List<Map> mockValidData() =>
+      [
+        {
+          'id': faker.guid.guid(),
+          'question': faker.lorem.random.string(10),
+          'dateTime': faker.date.dateTime(),
+          'didAnswer': false,
+        },
+        {
+          'id': faker.guid.guid(),
+          'question': faker.lorem.random.string(10),
+          'dateTime': faker.date.dateTime(),
+          'didAnswer': false,
+        }
+      ];
+
+  void mockFetch(List<Map> list) {
+    data = list;
+    when(fetchCacheStorage.fetch(any)).thenAnswer((_) async => data);
+  }
 
   setUp(() {
     fetchCacheStorage = FetchCacheStorageSpy();
     sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
-
+    mockFetch(mockValidData());
   });
 
   test('Should call FetchCacheStorage with correct key', () async {
-
     await sut.load();
 
     verify(fetchCacheStorage.fetch('surveys')).called(1);
+  });
+
+  test('Should return a list of surveys on success', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, [
+      SurveyEntity(
+          id: data[0]['id'] as String,
+          question: data[0]['question'] as String,
+          dateTime: data[0]['dateTime'] as DateTime,
+          didAnswer: data[0]['didAnswer'] as bool),
+      SurveyEntity(
+          id: data[1]['id'] as String,
+          question: data[1]['question'] as String,
+          dateTime: data[1]['dateTime'] as DateTime,
+          didAnswer: data[1]['didAnswer'] as bool),
+    ]);
   });
 }
