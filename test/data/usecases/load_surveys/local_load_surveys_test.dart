36d271ef4d3a43af62a428a8afd1a64afd257c07
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-08-22 08:13:14 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is null

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -14,7 +14,7 @@ class LocalLoadSurveys {
   Future<List<SurveyEntity>> load() async {
     final data = await fetchCacheStorage.fetch('surveys');
 
-    if(data?.isEmpty == true) {
+    if(data?.isEmpty != false) {
       throw DomainError.unexpected;
     }
 
@@ -89,6 +89,14 @@ void main() {
 
   test('Should throw UnexpectedError if cache is empty', () async {
     mockFetch([]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if cache is empty', () async {
+    mockFetch(null);
 
     final future = sut.load();
 
