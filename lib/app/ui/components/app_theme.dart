4ec48b6d57e2c6245d06c3e2cad20d87d722bd12
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-09-01 10:54:27 -0300

    Modulo 26 - Aula 02 - Add SurveyResult Layout

--- lib/app/ui/components/app_theme.dart
+++ lib/app/ui/components/app_theme.dart
@@ -4,15 +4,21 @@ ThemeData makeAppTheme() {
   const primaryColor = Color.fromRGBO(136, 14, 79, 1);
   const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
   const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+  const secundaryColor = Color.fromRGBO(0, 77, 64, 1);
   const secundaryColorDart = Color.fromRGBO(0, 37, 26, 1);
+  final disabledColor = Colors.grey[400];
+  final dividerColor = Colors.grey;
 
   return ThemeData(
     primaryColor: primaryColor,
     primaryColorDark: primaryColorDark,
     primaryColorLight: primaryColorLight,
+    highlightColor: secundaryColor,
     secondaryHeaderColor: secundaryColorDart,
     accentColor: primaryColor,
     backgroundColor: Colors.white,
+    disabledColor: disabledColor,
+    dividerColor: dividerColor,
     //scaffoldBackgroundColor: primaryColorDark, // -> works
     cardColor: primaryColorDark,
 
